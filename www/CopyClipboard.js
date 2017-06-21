4b525e66e6376f0f835ed889393e1476541252fd
HiroyukiWatase <eleven@HiroyukiWatasenoMacBook-Pro.local>
2017-06-21 18:26:57 +0800

    first

--- www/CopyClipboard.js
+++ www/CopyClipboard.js
@@ -1,5 +1,10 @@
-var exec = require('cordova/exec');
-
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "CopyClipboard", "coolMethod", [arg0]);
+module.exports = {
+  sayHello: function(name, successCallback, errorCallback) {
+    // 第1引数: 成功時に呼び出す関数
+    // 第2引数: エラー時に呼び出す関数
+    // 第3引数: プラグインの名前（plugin.xmlのfeatureのnameに設定したもの）
+    // 第4引数: HelloWorld.javaの第1引数に渡る名前
+    // 第5引数: HelloWorld.javaの第2引数に渡る値
+    cordova.exec(successCallback, errorCallback, "copyclipboard", "copy", [name]);
+  }
 };
