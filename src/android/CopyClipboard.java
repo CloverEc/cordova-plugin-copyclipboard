37611bad3482b63fecb0b54bf5f62506ba3ad0c1
HiroyukiWatase <eleven@HiroyukiWatasenoMacBook-Pro.local>
2017-06-21 19:26:51 +0800

    add clipboard

--- src/android/CopyClipboard.java
+++ src/android/CopyClipboard.java
@@ -3,16 +3,20 @@ package plugin.copyclipboard;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import android.content.Context;
+import android.content.ClipboardManager;
+import android.content.ClipData;
 
 public class CopyClipboard extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
+      ClipboardManager clipboard = (ClipboardManager) cordova.getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
         if (action.equals("copy")) {
-            String name = data.getString(0);
-            String message = "Hello, World !!! " + "Hello, " + name;
-            callbackContext.success(message);
+            String str = data.getString(0);
+            ClipData clip = ClipData.newPlainText("Text", str);
+            clipboard.setPrimaryClip(clip);
+            callbackContext.success(str);
             return true;
         } else {
             return false;
